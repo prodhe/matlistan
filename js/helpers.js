c96c875909c71b49ac03c22ad93951762a61b489
Petter Rodhelind <petter.rodhelind@gmail.com>
2016-05-05 23:01:46 +0200

    separated helpers into new file

---
+++ js/helpers.js
@@ -0,0 +1,21 @@
+/* helper functions */
+
+function sortAlpha(a, b) {
+	return a.toLowerCase() > b.toLowerCase() ? 1 : -1;
+}
+
+function safe2html(str) {
+    str = str.replace(/</g,"&lt;");
+    str = str.replace(/>/g,"&gt;");
+    return str;
+}
+
+function addObjToList(obj) {
+	$(obj).clone().appendTo($('#userlist'));
+	$('#userlist').trigger("sortupdate");
+}
+
+function clearUserlist() {
+	$('#userlist').children().remove();
+	$('#userlist').trigger("sortupdate");
+}

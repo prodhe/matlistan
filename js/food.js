a700288c225f7f60948c8593736429210c2a7abc
Petter Rodhelind <petter.rodhelind@gmail.com>
2016-05-06 12:56:07 +0200

    moved helpers function to separate file

--- js/food.js
+++ js/food.js
@@ -3,24 +3,6 @@
 /* global storage for loaded json data */
 window.data = [];
 
-/* helper functions */
-function sortAlpha(a, b) {
-    return (a.toLowerCase() > b.toLowerCase()) ? 1 : -1;
-}
-function safe2html(str) {
-    str = str.replace(/</g, "&lt;");
-    str = str.replace(/>/g, "&gt;");
-    return str;
-}
-function addObjToList(obj) {
-    $(obj).clone().appendTo($('#userlist'));
-    $('#userlist').trigger("sortupdate");
-}
-function clearUserlist() {
-    $('#userlist').children().remove();
-    $('#userlist').trigger("sortupdate");
-}
-
 /* fill main list with items */
 function loadList(obj) {
     $('#mainlist').children().remove();
--- js/helpers.js
+++ js/helpers.js
@@ -1,21 +1,21 @@
 /* helper functions */
 
 function sortAlpha(a, b) {
-	return a.toLowerCase() > b.toLowerCase() ? 1 : -1;
+    return (a.toLowerCase() > b.toLowerCase()) ? 1 : -1;
 }
 
 function safe2html(str) {
-    str = str.replace(/</g,"&lt;");
-    str = str.replace(/>/g,"&gt;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/>/g, "&gt;");
     return str;
 }
 
 function addObjToList(obj) {
-	$(obj).clone().appendTo($('#userlist'));
-	$('#userlist').trigger("sortupdate");
+    $(obj).clone().appendTo($('#userlist'));
+    $('#userlist').trigger("sortupdate");
 }
 
 function clearUserlist() {
-	$('#userlist').children().remove();
-	$('#userlist').trigger("sortupdate");
+    $('#userlist').children().remove();
+    $('#userlist').trigger("sortupdate");
 }

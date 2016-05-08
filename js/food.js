07427a4f8a87b8bb07615c95f4121b6c041439de
Petter Rodhelind <petter.rodhelind@gmail.com>
2016-05-08 13:16:04 +0200

    Indentation stream lining.

--- js/food.js
+++ js/food.js
@@ -1,32 +1,34 @@
-/* food.js */
+/*
+ * food.js
+ * */
 
-/* global storage for loaded json data */
+// global storage for loaded json data
 window.data = [];
 
-/* fill main list with items */
+// fill main list with items
 function loadList(obj) {
     $('#mainlist').children().remove();
     var data = obj.dishes;
     window.data = data;
     data.forEach(function (item) {
-        $('#mainlist').append(
-            $('<li>')
+        $('#mainlist')
+            .append(
+                $('<li>')
                 .addClass("list-group-item")
                 .draggable({
                     helper: 'clone',
                     addClasses: false,
                     stack: ".list-group-item"
-                    //connectToSortable: "#userlist"
                 })
                 .dblclick(function () {
                     addObjToList(this);
                 })
                 .append(safe2html(item.name))
-        );
+            );
     });
 }
 
-/* update ingredients list */
+// update ingredients list
 function updateIngredients() {
     // clear current list
     $('#ingredientslist').children().remove();
@@ -60,14 +62,13 @@ function updateIngredients() {
         }
     });
 
-    // remove duplicates from ingredients list,
-    // now that we have a proper count
+    // remove duplicates from ingredients list, now that we have a proper count
     ingredients = ingredients.filter(function (el, i, a) {
         return (i === a.indexOf(el)) ? 1 : 0;
     });
 
-    // add ingredients to the visual HTML list
-    // and add click function for ignoring item before saving
+    // add ingredients to the visual HTML list and add click function for
+    // ignoring item before saving
     var i;
     for (i = 0; i < ingredients.length; i += 1) {
         var ing_name = ingredients[i];
@@ -82,14 +83,14 @@ function updateIngredients() {
         }).append(safe2html(txt)));
     }
 
-    // for visual purposes, make sure there's always 5 rows
-    // in the list (or the same as in the HTML file)
+    // for visual purposes, make sure there's always 5 rows in the list (or the
+    // same as in the HTML file)
     while ($('#ingredientslist > li').length < 5) {
         $('#ingredientslist').append($('<li>'));
     }
 }
 
-/* randomize the user list with n elements from main */
+// randomize the user list with n elements from main
 function randomUserlist(n) {
     // abort if not numeric
     if (!($.isNumeric(n))) {
@@ -108,8 +109,8 @@ function randomUserlist(n) {
         // if it's already in the list, add 1 (or circle around to beginning)
         // and check again
         while ($.inArray(rand, memory) !== -1) {
-            // if we want more random items than length of mainlist,
-            // clear the memory if it's already full
+            // if we want more random items than length of mainlist, clear the
+            // memory if it's already full
             if (memory.length === mainlength) {
                 memory = [];
             }
@@ -120,75 +121,81 @@ function randomUserlist(n) {
             }
         }
 
-        // finally, remember the item number for next run and
-        // add it to the visual list
+        // finally, remember the item number for next run and add it to the
+        // visual list
         addObjToList($('#mainlist > li:nth-child(' + rand + ')'));
         memory.push(rand);
     }
 }
 
-/* onload */
+// onload
 $(document).ready(function () {
-    // load food data and add to main list
-    /*$.ajax({
-        url: "./data.json",
-        success: function(file) {
-            var a = file.dishes;
-            for (var i=0; i<a.length; i++) {
-                if ("name" in a[i] && "ingredients" in a[i]) {
-                    // clear out empty string ingredients
-                    a[i]["ingredients"] = a[i]["ingredients"].filter(function(el,i,a){
-                        return (el != "") ? 1 : 0;
-                    });
-                    // add to global storage
-                    window.data.push(a[i]);
-                }
-            }
-            // sort main list
-            window.data.sort(function(a,b) {
-                return sortAlpha(a["name"],b["name"]);
-            });
-            // load to HTML
-            loadList({"dishes":window.data});
-        }
-    });*/
+
+    // // load food data and add to main list
+    // $.ajax({
+    //     url: "./data.json",
+    //     success: function (file) {
+    //         var a = file.dishes;
+    //         for (var i=0; i<a.length; i++) {
+    //             if ("name" in a[i] && "ingredients" in a[i]) {
+    //                 // clear out empty string ingredients
+    //                 a[i]["ingredients"] = a[i]["ingredients"].filter(function(el,i,a){
+    //                     return (el != "") ? 1 : 0;
+    //                 });
+    //                 // add to global storage
+    //                 window.data.push(a[i]);
+    //             }
+    //         }
+    //         // sort main list
+    //         window.data.sort(function(a,b) {
+    //             return sortAlpha(a["name"],b["name"]);
+    //         });
+    //         // load to HTML
+    //         loadList({"dishes":window.data});
+    //     }
+    // });
 
     // make items in userlist sortable
-    $('#userlist').sortable({
-        revert: 100,
-        forcePlaceholderSize: true
-    }).droppable({
-        accept: "#mainlist > li",
-        over: function (e, ui) {
-            $(this).fadeTo(200, 0.3);
-        },
-        out: function (e, ui) {
-            $(this).fadeTo(200, 1.0);
-        },
-        drop: function (e, ui) {
-            addObjToList(ui.draggable);
-            ui.helper.remove();
-            $(this).fadeTo(200, 1.0);
-        }
-    }).disableSelection();
+    $('#userlist')
+        .sortable({
+            revert: 100,
+            forcePlaceholderSize: true
+        })
+        .droppable({
+            accept: "#mainlist > li",
+            over: function (e, ui) {
+                $(this).fadeTo(200, 0.3);
+            },
+            out: function (e, ui) {
+                $(this).fadeTo(200, 1.0);
+            },
+            drop: function (e, ui) {
+                addObjToList(ui.draggable);
+                ui.helper.remove();
+                $(this).fadeTo(200, 1.0);
+            }
+        })
+        .disableSelection();
 
-    // make the mainlist a valid drop target
-    // for dragged items in userlist
-    $('#mainlist').droppable({
-        accept: "#userlist > li",
-        over: function (e, ui) {
-            $(this).fadeTo(200, 0.3);
-        },
-        out: function (e, ui) {
-            $(this).fadeTo(200, 1.0);
-        },
-        drop: function (e, ui) {
-            ui.draggable.remove();
-            $(this).fadeTo(200, 1.0);
-        }
-    }).disableSelection();
-    // need to bind update event function after initialization
-    // for the outside trigger function to work properly...
+    // make the mainlist a valid drop target for dragged items in userlist
+    $('#mainlist')
+        .droppable({
+            accept: "#userlist > li",
+            over: function (e, ui) {
+                $(this).fadeTo(200, 0.3);
+            },
+            out: function (e, ui) {
+                $(this).fadeTo(200, 1.0);
+            },
+            drop: function (e, ui) {
+                ui.draggable.remove();
+                $(this).fadeTo(200, 1.0);
+            }
+        })
+        .disableSelection();
+
+    // need to bind update event function after initialization for the outside
+    // trigger function to work properly...
     $('#userlist').bind("sortupdate", function (e, ui) {
         $('#userlist > li').dblclick(function () {
             $(this).remove();

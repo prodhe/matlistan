6e9d8232ccdece573caa329d74f89ef4b142106d
Petter Rodhelind <petter.rodhelind@gmail.com>
2017-09-24 23:35:00 +0200

    Run goimports/gofmt.

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"os"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-    if port == "" {
-        port = "8000"
-    }
+	if port == "" {
+		port = "8000"
+	}
 	fmt.Println("Listening on port", port)
-    err := http.ListenAndServe(":"+port, http.FileServer(http.Dir("./web/")))
+	err := http.ListenAndServe(":"+port, http.FileServer(http.Dir("./web/")))
 	if err != nil {
 		fmt.Println(err)
 	}
 }
-

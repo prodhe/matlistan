f4f9994ea218915e41204c98be8c57a6d8fdd107
Petter Rodhelind <petter.rodhelind@gmail.com>
2020-01-23 20:28:39 +0100

    sort: Fix bug in recipe category sorting.

--- model/sort.go
+++ model/sort.go
@@ -10,6 +10,14 @@ func (v RecipesByTitle) Less(i, j int) bool { return v[i].Title < v[j].Title }
 // RecipesByCategory sorts recipes by primary category
 type RecipesByCategory []Recipe
 
-func (v RecipesByCategory) Len() int           { return len(v) }
-func (v RecipesByCategory) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
-func (v RecipesByCategory) Less(i, j int) bool { return v[i].Categories[0] < v[j].Categories[0] }
+func (v RecipesByCategory) Len() int      { return len(v) }
+func (v RecipesByCategory) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
+func (v RecipesByCategory) Less(i, j int) bool {
+	if len(v[i].Categories) == 0 && len(v[j].Categories) > 0 {
+		return true
+	}
+	if len(v[j].Categories) == 0 {
+		return false
+	}
+	return v[i].Categories[0] < v[j].Categories[0]
+}

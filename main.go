fc91daa4633780f9063cb8cbe52d467c3f949d4f
Petter Rodhelind <prd@vty.se>
2022-06-07 22:31:27 +0200

    Remove db dependency and make non-account public.

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	mgo "gopkg.in/mgo.v2"
-
 	"os"
 
 	"github.com/prodhe/matlistan/handler"
@@ -26,11 +24,7 @@ func main() {
 		"StaticDir": "/static",
 	}
 	template.Load("index", df, "base.html", "index.html")
-	template.Load("signup", df, "base.html", "signup.html")
-	template.Load("login", df, "base.html", "login.html")
 	template.Load("help", df, "base.html", "help.html")
-	template.Load("profile", df, "base.html", "profile.html")
-	template.Load("recipes", df, "base.html", "recipes.html")
 
 	if os.Getenv("DEVMODE") != "" {
 		log.Println("Development mode")
@@ -42,20 +36,7 @@ func main() {
 		port = "8000"
 	}
 
-	mongodbURI := os.Getenv("MONGODB_URI")
-	if mongodbURI == "" {
-		mongodbURI = "mongodb://localhost:27017/foodlist"
-	}
-
-	session, err := mgo.Dial(mongodbURI)
-	if err != nil {
-		log.Fatalf("Could not dial mongo db: %v\n", err)
-	}
-	defer session.Close()
-	session.SetMode(mgo.Monotonic, true)
-	log.Println("Connected to MongoDB")
-
-	h := handler.New(session.DB(""))
+	h := handler.New()
 
 	log.Println("Listening on port", port)
 	if err := http.ListenAndServe(":"+port, h); err != nil {
